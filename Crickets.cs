32216dde060c85a421ba1d3a82f6674786c48eab
123abortik <rainjuicy1@gmail.com>
2024-11-10 15:28:43 +0300

    Update Crickets.cs

--- Crickets.cs
+++ Crickets.cs
@@ -7,7 +7,7 @@ public class Crickets : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       print("crickets"); 
+       
     }
 
     // Update is called once per frame
